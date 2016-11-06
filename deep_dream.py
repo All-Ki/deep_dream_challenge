d2df7075132e33958d4cb7082212bc6dd39df039
Peter Mitrano <mitranopeter@gmail.com>
2016-11-06 01:03:01 -0400

    something fucky happening with tf sess
    but it does server deepdreamed images from live webcam

--- deep_dream.py
+++ deep_dream.py
@@ -8,9 +8,78 @@ import urllib
 import os
 import zipfile
 
-output_image_counter = 0
+def T(graph, layer):
+    '''Helper for getting layer output tensor'''
+    return graph.get_tensor_by_name("import/%s:0"%layer)
 
-def main(input_filename):
+def tffunc(*argtypes):
+    '''Helper that transforms TF-graph generating function into a regular one.
+    See "resize" function below.
+    '''
+    placeholders = list(map(tf.placeholder, argtypes))
+    def wrap(f):
+        out = f(*placeholders)
+        def wrapper(*args, **kw):
+            return out.eval(dict(zip(placeholders, args)), session=kw.get('session'))
+        return wrapper
+    return wrap
+
+def resize(img, size):
+    img = tf.expand_dims(img, 0)
+    return tf.image.resize_bilinear(img, size)[0,:,:,:]
+
+resize = tffunc(np.float32, np.int32)(resize)
+
+def calc_grad_tiled(sess, img, t_grad, t_input, tile_size=512):
+    '''Compute the value of tensor t_grad over the image in a tiled way.
+    Random shifts are applied to the image to blur tile boundaries over
+    multiple iterations.'''
+    sz = tile_size
+    h, w = img.shape[:2]
+    sx, sy = np.random.randint(sz, size=2)
+    img_shift = np.roll(np.roll(img, sx, 1), sy, 0)
+    grad = np.zeros_like(img)
+    for y in range(0, max(h-sz//2, sz),sz):
+        for x in range(0, max(w-sz//2, sz),sz):
+            sub = img_shift[y:y+sz,x:x+sz]
+            g = sess.run(t_grad, {t_input:sub})
+            grad[y:y+sz,x:x+sz] = g
+    return np.roll(np.roll(grad, -sx, 1), -sy, 0)
+
+def visstd(a, s=0.1):
+    '''Normalize the image range for visualization'''
+    return (a-a.mean())/max(a.std(), 1e-4)*s + 0.5
+
+def render_deepdream(sess, t_obj, t_input, img0,
+                     iter_n=10, step=1.5, octave_n=2, octave_scale=1.4):
+    t_score = tf.reduce_mean(t_obj) # defining the optimization objective
+    t_grad = tf.gradients(t_score, t_input)[0] # behold the power of automatic differentiation!
+
+    # split the image into a number of octaves
+    img = img0
+    octaves = []
+    for _ in range(octave_n-1):
+        hw = img.shape[:2]
+        lo = resize(img, np.int32(np.float32(hw)/octave_scale))
+        hi = img-resize(lo, hw)
+        img = lo
+        octaves.append(hi)
+
+    # generate details octave by octave
+    for octave in range(octave_n):
+        if octave>0:
+            hi = octaves[-octave]
+            img = resize(img, hi.shape[:2])+hi
+        for _ in range(iter_n):
+            g = calc_grad_tiled(sess, img, t_grad, t_input)
+            img += g*(step / (np.abs(g).mean()+1e-7))
+
+    #Step 5 return frame
+    output_frame = img / 255.0
+    output_frame = np.uint8(np.clip(output_frame, 0, 1)*255)
+    return output_frame
+
+def setup():
     #Step 1 - download google's pre-trained neural network
     url = 'https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip'
     data_dir = 'data/'
@@ -51,148 +120,35 @@ def main(input_filename):
     print('Number of layers', len(layers))
     print('Total number of feature channels:', sum(feature_nums))
 
- #####HELPER FUNCTIONS. I didn't go over these in the video for times sake. They are mostly just formatting functions. Scroll
- #to the bottom #########################################################################################################
- ########################################################################################################################
- ############################################################
-
-    # Helper functions for TF Graph visualization
-    #pylint: disable=unused-variable
-    def strip_consts(graph_def, max_const_size=32):
-        """Strip large constant values from graph_def."""
-        strip_def = tf.GraphDef()
-        for n0 in graph_def.node:
-            n = strip_def.node.add() #pylint: disable=maybe-no-member
-            n.MergeFrom(n0)
-            if n.op == 'Const':
-                tensor = n.attr['value'].tensor
-                size = len(tensor.tensor_content)
-                if size > max_const_size:
-                    tensor.tensor_content = "<stripped %d bytes>"%size
-        return strip_def
-
-    def rename_nodes(graph_def, rename_func):
-        res_def = tf.GraphDef()
-        for n0 in graph_def.node:
-            n = res_def.node.add() #pylint: disable=maybe-no-member
-            n.MergeFrom(n0)
-            n.name = rename_func(n.name)
-            for i, s in enumerate(n.input):
-                n.input[i] = rename_func(s) if s[0]!='^' else '^'+rename_func(s[1:])
-        return res_def
-
-    def visstd(a, s=0.1):
-        '''Normalize the image range for visualization'''
-        return (a-a.mean())/max(a.std(), 1e-4)*s + 0.5
-
-    def T(layer):
-        '''Helper for getting layer output tensor'''
-        return graph.get_tensor_by_name("import/%s:0"%layer)
-
-    def render_naive(t_obj, img0=img_noise, iter_n=20, step=1.0):
-        t_score = tf.reduce_mean(t_obj) # defining the optimization objective
-        t_grad = tf.gradients(t_score, t_input)[0] # behold the power of automatic differentiation!
-
-        img = img0.copy()
-        for _ in range(iter_n):
-            g, _ = sess.run([t_grad, t_score], {t_input:img})
-            # normalizing the gradient, so the same step size should work
-            g /= g.std()+1e-8         # for different layers and networks
-            img += g*step
-        showarray(visstd(img))
-
-    def tffunc(*argtypes):
-        '''Helper that transforms TF-graph generating function into a regular one.
-        See "resize" function below.
-        '''
-        placeholders = list(map(tf.placeholder, argtypes))
-        def wrap(f):
-            out = f(*placeholders)
-            def wrapper(*args, **kw):
-                return out.eval(dict(zip(placeholders, args)), session=kw.get('session'))
-            return wrapper
-        return wrap
-
-    def resize(img, size):
-        img = tf.expand_dims(img, 0)
-        return tf.image.resize_bilinear(img, size)[0,:,:,:]
-    resize = tffunc(np.float32, np.int32)(resize)
-
-    def calc_grad_tiled(img, t_grad, tile_size=512):
-        '''Compute the value of tensor t_grad over the image in a tiled way.
-        Random shifts are applied to the image to blur tile boundaries over
-        multiple iterations.'''
-        sz = tile_size
-        h, w = img.shape[:2]
-        sx, sy = np.random.randint(sz, size=2)
-        img_shift = np.roll(np.roll(img, sx, 1), sy, 0)
-        grad = np.zeros_like(img)
-        for y in range(0, max(h-sz//2, sz),sz):
-            for x in range(0, max(w-sz//2, sz),sz):
-                sub = img_shift[y:y+sz,x:x+sz]
-                g = sess.run(t_grad, {t_input:sub})
-                grad[y:y+sz,x:x+sz] = g
-        return np.roll(np.roll(grad, -sx, 1), -sy, 0)
-
-    #BACK TO CODE IN THE VIDEO###########################################################################################
-    ########################################################################################################
-    ##############################################################################
-
-    #CHALLENGE - Write a function that outputs a deep dream video
-    #def render_deepdreamvideo():
-
-
-    def render_deepdream(t_obj, img0=img_noise,
-                         iter_n=10, step=1.5, octave_n=8, octave_scale=1.4):
-        t_score = tf.reduce_mean(t_obj) # defining the optimization objective
-        t_grad = tf.gradients(t_score, t_input)[0] # behold the power of automatic differentiation!
-
-        # split the image into a number of octaves
-        img = img0
-        octaves = []
-        for _ in range(octave_n-1):
-            hw = img.shape[:2]
-            lo = resize(img, np.int32(np.float32(hw)/octave_scale))
-            hi = img-resize(lo, hw)
-            img = lo
-            octaves.append(hi)
-
-        # generate details octave by octave
-        for octave in range(octave_n):
-            if octave>0:
-                hi = octaves[-octave]
-                img = resize(img, hi.shape[:2])+hi
-            for _ in range(iter_n):
-                g = calc_grad_tiled(img, t_grad)
-                img += g*(step / (np.abs(g).mean()+1e-7))
-
-        #Step 5 return frame
-        output_frame = img / 255.0
-        output_frame = np.uint8(np.clip(output_frame, 0, 1)*255)
-        return output_frame
-
-
-
    	#Step 3 - Pick a layer to enhance our image
     layer = 'mixed4d_3x3_bottleneck_pre_relu'
-    channel = 139 # picking some feature channel to visualize
+    channel = 129 # picking some feature channel to visualize
 
-    #open video file
-    cap = cv2.VideoCapture(input_filename)
+    return sess, graph, t_input
+
+def run(sess, graph, t_input, input_filename):
+    #open webcam
+    cap = cv2.VideoCapture(0)
 
     writer = None
     i = 0
-    while(cap.isOpened()):
+    LIM_FRAMES = 12
+    while cap.isOpened() and i < LIM_FRAMES:
         ret, frame = cap.read()
 
         if frame is None:
             break
 
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        frame = np.float32(frame)
+
         #Step 4 - Apply gradient ascent to that layer
-        output_frame = render_deepdream(tf.square(T('mixed4c')), frame)
+        output_frame = render_deepdream(sess, tf.square(T(graph, 'mixed4c')), t_input, frame)
         if writer is None:
             frame_size = (output_frame.shape[1], output_frame.shape[0])
-            writer = cv2.VideoWriter('output.avi', cv2.cv.FOURCC(*'XVID'), 20, frame_size)
+            writer = cv2.VideoWriter('output.avi', cv2.cv.FOURCC(*'XVID'), 4, frame_size)
 
         writer.write(output_frame)
         i += 1
@@ -201,11 +157,12 @@ def main(input_filename):
     cap.release()
 
 
-
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         input_filename = 'input.avi'
     else:
         input_filename = sys.argv[1]
 
-    main(input_filename)
+    sess, graph, t_input = setup()
+    run(sess, graph, input_filename)
+
